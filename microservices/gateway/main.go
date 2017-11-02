2de13c6d62243c2fbff4501711452151ff485abd
Melody Lee <leemeli@uw.edu>
2017-11-02 13:36:09 -0700

    10/31

--- microservices/gateway/main.go
+++ microservices/gateway/main.go
@@ -4,8 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"os"
+	"strings"
+	"sync"
 )
+
+func NewServiceProxy(addrs []string) *httputil.ReverseProxy {
+	nextIndex := 0
+	mx := sync.Mutex{}
+	return &httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			//modify the request to indicate
+			//remote host
+			mx.Lock()
+			r.URL.Host = addrs[nextIndex%len(addrs)]
+			nextIndex++
+			mx.Unlock()
+			r.URL.Scheme = "http"
+		},
+	}
+}
 
 //RootHandler handles requests for the root resource
 func RootHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,9 +40,15 @@ func main() {
 
 	//TODO: get network addresses for our
 	//timesvc instances
+	timesvcAddrs := os.Getenv("TIMESVC_ADDRS")
+	splitTimeSvcAddr := strings.Split(timesvcAddrs, ",")
+	hellosvcAddrs := os.Getenv("HELLO_ADDRS")
+	splitHelloSvcAddr := strings.Split(hellosvcAddrs, ",")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", RootHandler)
+	mux.Handle("/v1/time", NewServiceProxy(splitTimeSvcAddr))
+	mux.Handle("/v1/hello", NewServiceProxy(splitHelloSvcAddr))
 	//TODO: add reverse proxy handler for `/v1/time`
 
 	log.Printf("server is listening at https://%s...", addr)
