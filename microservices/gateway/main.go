deb1e6c3e529a0cea0925541e6ccc6995c9abf8a
Melody Lee <leemeli@uw.edu>
2017-11-06 14:09:55 -0800

    11/2

--- microservices/gateway/main.go
+++ microservices/gateway/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,19 @@ import (
 	"sync"
 )
 
+type User struct {
+	FirstName string `json:"firstName,omitempty"`
+	LastName  string `json:"lastName,omitempty"`
+}
+
+func GetCurrentUser(r *http.Request) *User {
+	//does some magic with our sessions packages
+	return &User{
+		FirstName: "Test",
+		LastName:  "User",
+	}
+}
+
 func NewServiceProxy(addrs []string) *httputil.ReverseProxy {
 	nextIndex := 0
 	mx := sync.Mutex{}
@@ -17,6 +31,12 @@ func NewServiceProxy(addrs []string) *httputil.ReverseProxy {
 		Director: func(r *http.Request) {
 			//modify the request to indicate
 			//remote host
+			user := GetCurrentUser(r)
+			userJSON, err := json.Marshal(user)
+			if err != nil {
+				log.Printf("error marshaling user: %v", err)
+			}
+			r.Header.Add("X-User", string(userJSON))
 			mx.Lock()
 			r.URL.Host = addrs[nextIndex%len(addrs)]
 			nextIndex++
@@ -42,13 +62,19 @@ func main() {
 	//timesvc instances
 	timesvcAddrs := os.Getenv("TIMESVC_ADDRS")
 	splitTimeSvcAddr := strings.Split(timesvcAddrs, ",")
+
 	hellosvcAddrs := os.Getenv("HELLO_ADDRS")
 	splitHelloSvcAddr := strings.Split(hellosvcAddrs, ",")
+
+	nodesvcAddrs := os.Getenv("NODESVC_ADDRS")
+	splitNodeSvcAddr := strings.Split(nodesvcAddrs, ",")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", RootHandler)
 	mux.Handle("/v1/time", NewServiceProxy(splitTimeSvcAddr))
 	mux.Handle("/v1/hello", NewServiceProxy(splitHelloSvcAddr))
+	mux.Handle("/v1/users/me/", NewServiceProxy(splitNodeSvcAddr))
+	mux.Handle("/v1/channels/", NewServiceProxy(splitNodeSvcAddr))
 	//TODO: add reverse proxy handler for `/v1/time`
 
 	log.Printf("server is listening at https://%s...", addr)
